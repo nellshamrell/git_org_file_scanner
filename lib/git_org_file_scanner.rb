65c77d4399ec432420c82e6fe62c7bbc3749bd0e
Nell Shamrell <nellshamrell@gmail.com>
2018-11-10 15:39:27 -0800

    initial functionality

--- lib/git_org_file_scanner.rb
+++ lib/git_org_file_scanner.rb
@@ -1,5 +1,23 @@
 require "git_org_file_scanner/version"
+require "octokit"
 
 module GitOrgFileScanner
-  # Your code goes here...
+  class Scanner
+    attr_accessor :org
+
+    def initialize(org)
+      @octokit_client = Octokit::Client.new
+      @org = org
+    end
+
+    def check_for_file(file)
+      org_repositories.each do |repo|
+        @octokit_client.contents(repo[:full_name], path: file)
+      end
+    end
+
+    def org_repositories
+      @octokit_client.org_repositories(org)
+    end
+  end
 end
--- spec/git_org_file_scanner_spec.rb
+++ spec/git_org_file_scanner_spec.rb
@@ -3,7 +3,50 @@ RSpec.describe GitOrgFileScanner do
     expect(GitOrgFileScanner::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  context "connecting to github" do
+    let(:org) { 'my-org' }
+
+    it "initialize a connection to the GitHub API" do
+      expect(Octokit::Client).to receive(:new)
+
+      GitOrgFileScanner::Scanner.new(org)
+    end
+  end
+
+  context 'getting org repos' do
+    let(:org) { 'my-org' }
+    let(:scanner) { GitOrgFileScanner::Scanner.new(org)}
+    let(:github_client) { Octokit::Client.new }
+    
+    before do
+      allow(Octokit::Client).to receive(:new).and_return(github_client)
+    end
+
+    it 'gets a list of repos in the org' do
+      expect(github_client).to receive(:org_repositories).with(org)
+
+      scanner.org_repositories
+    end
+  end
+
+  context 'scanning org repos for a file' do
+    let(:org) { 'my-org' }
+    let(:org_repos_response) do 
+      [{id: 36098429, name: "habitat", full_name:"habitat-sh/habitat"},{id: 116978574, name: "national-parks", full_name: "habitat-sh/national-parks"}]
+    end
+    let(:scanner) { GitOrgFileScanner::Scanner.new(org)}
+    let(:github_client) { Octokit::Client.new }
+    let(:file) { 'CONTRIBUTING.md' }
+
+    before do
+      allow(Octokit::Client).to receive(:new).and_return(github_client)
+      allow(github_client).to receive(:org_repositories).and_return(org_repos_response)
+    end
+
+    it 'checks each repo for the file' do
+      expect(github_client).to receive(:contents).with('habitat-sh/habitat', path: file).ordered
+      expect(github_client).to receive(:contents).with('habitat-sh/national-parks', path: file).ordered
+      scanner.check_for_file(file)
+    end
   end
 end
